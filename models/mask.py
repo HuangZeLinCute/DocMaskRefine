cb5a4217741b6bea9e9deafb2dbd82c2fa7c2aa0
Zelin Huang <122612645+HuangZeLinCute@users.noreply.github.com>
2025-10-09 09:05:24 +0800

    Update mask.py
    bug

--- models/mask.py
+++ models/mask.py
@@ -2,8 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import einops
-from models.EA import EdgeAwareLayer
-
 
 # ========================
 # LayerNorm (BiasFree / WithBias)
